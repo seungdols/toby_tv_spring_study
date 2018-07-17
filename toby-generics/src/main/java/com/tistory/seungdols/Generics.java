cb6c50d50cc8408ec279e1f3b4c400d0437c84fc
seungdols <seungdols0822@gmail.com>
2018-07-18 01:23:00 +0900

    Raw Type 설명

--- toby-generics/src/main/java/com/tistory/seungdols/Generics.java
+++ toby-generics/src/main/java/com/tistory/seungdols/Generics.java
@@ -1,5 +1,9 @@
 package com.tistory.seungdols;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @PACKAGE com.tistory.seungdols
  * @AUTHOR seungdols
@@ -16,6 +20,14 @@ public class Generics {
 
 
     public static void main(String[] args) {
-        new Hello<String>(); //type argument
+        //Raw Type = Generic으로 타입을 주고 했으나, 받을때 타입 정보를 명시 해주지 않음.
+        List list = new ArrayList<Integer>();
+
+        List<Integer> ints = Arrays.asList(1, 2, 3);
+        List rawInts = ints;
+        List<Integer> ints2 = rawInts;
+        //compile error는 발생하지 않는다.
+        List<String> strs = rawInts;
+        String str = strs.get(0);
     }
 }
