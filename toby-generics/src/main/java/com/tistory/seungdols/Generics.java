1448f0cae68708c1d9216d2b1a910a42f991ff1a
seungdols <seungdols0822@gmail.com>
2018-07-19 00:16:22 +0900

    SubType에 대한 설명

--- toby-generics/src/main/java/com/tistory/seungdols/Generics.java
+++ toby-generics/src/main/java/com/tistory/seungdols/Generics.java
@@ -1,7 +1,6 @@
 package com.tistory.seungdols;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @PACKAGE com.tistory.seungdols
@@ -10,17 +9,128 @@ import java.util.List;
  */
 public class Generics {
 
+    static class MyList<E, P> implements List<E> {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator<E> iterator() {
+            return null;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            return null;
+        }
+
+        @Override
+        public boolean add(E e) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends E> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(int index, Collection<? extends E> c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public E get(int index) {
+            return null;
+        }
+
+        @Override
+        public E set(int index, E element) {
+            return null;
+        }
+
+        @Override
+        public void add(int index, E element) {
+
+        }
+
+        @Override
+        public E remove(int index) {
+            return null;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public ListIterator<E> listIterator() {
+            return null;
+        }
+
+        @Override
+        public ListIterator<E> listIterator(int index) {
+            return null;
+        }
+
+        @Override
+        public List<E> subList(int fromIndex, int toIndex) {
+            return null;
+        }
+    }
+
     public static void main(String[] args) {
-        Integer integer = 10;
-        Number number = integer;
+        List<String> s1 = new MyList<String, Integer>();
+        List<String> s2 = new MyList<String, String>();
 
-        List<Integer> integerList = new ArrayList<>();
-//        Quiz1)
-//        List<Number> numberList = integerList;
-
-        ArrayList<Integer> arrayList = new ArrayList<>();
-//        Quiz2)
-//        List<Integer> integers = arrayList;
+        
     }
 
 }
