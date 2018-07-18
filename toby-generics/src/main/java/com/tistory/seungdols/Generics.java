31d1cd064ea12f750058b8060f2ef39852d8da4a
seungdols <seungdols0822@gmail.com>
2018-07-18 23:57:45 +0900

    argument로 받은 List내 특정 범위 이상인 값 세기

--- toby-generics/src/main/java/com/tistory/seungdols/Generics.java
+++ toby-generics/src/main/java/com/tistory/seungdols/Generics.java
@@ -2,6 +2,7 @@ package com.tistory.seungdols;
 
 import java.io.Closeable;
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -12,17 +13,14 @@ import java.util.List;
  * @DATE 2018. 7. 18.
  */
 public class Generics {
-    //Bounded Type Parameter
-
-    static <T extends List & Serializable & Comparable & Closeable> void print(T t) { //Multiple Bounded
-
+    static long countGreaterThan(Integer[] arr, Integer elem) {
+        return Arrays.stream(arr).filter(s -> s > elem).count();
     }
 
     public static void main(String[] args) {
-
+        Integer[] arr = new Integer[] {1,2,3,4,5,6,7,8,9};
+        System.out.println(countGreaterThan(arr, 4));
     }
-
-
 
 
 }
