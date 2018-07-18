129724f5a7b08d9171ca11e3af687da15c4ffe0e
seungdols <seungdols0822@gmail.com>
2018-07-18 23:31:50 +0900

    Method Level에서 Type paramter 정의 예시

--- toby-generics/src/main/java/com/tistory/seungdols/Generics.java
+++ toby-generics/src/main/java/com/tistory/seungdols/Generics.java
@@ -10,24 +10,16 @@ import java.util.List;
  * @DATE 2018. 7. 18.
  */
 public class Generics {
-    static class Hello<T> { //T -> type parameter
 
+    <T> void print(T t) { // Method Level에 Type Paramter
+        System.out.println(t.toString());
     }
-
-    static void print(String value) {
-        System.out.println(value);
-    }
-
 
     public static void main(String[] args) {
-        //Raw Type = Generic으로 타입을 주고 했으나, 받을때 타입 정보를 명시 해주지 않음.
-        List list = new ArrayList<Integer>();
+        new Generics().print("Hello");
+        new Generics().print("Seungdols Company");
 
-        List<Integer> ints = Arrays.asList(1, 2, 3);
-        List rawInts = ints;
-        List<Integer> ints2 = rawInts;
-        //compile error는 발생하지 않는다.
-        List<String> strs = rawInts;
-        String str = strs.get(0);
     }
+
+
 }
