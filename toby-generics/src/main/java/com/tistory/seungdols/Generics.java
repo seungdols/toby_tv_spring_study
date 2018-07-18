618bdad5551e7693edfeb5e69f742e33b1d32753
seungdols <seungdols0822@gmail.com>
2018-07-19 00:21:05 +0900

    Generic에 관한 타입 추론 예시

--- toby-generics/src/main/java/com/tistory/seungdols/Generics.java
+++ toby-generics/src/main/java/com/tistory/seungdols/Generics.java
@@ -9,128 +9,16 @@ import java.util.*;
  */
 public class Generics {
 
-    static class MyList<E, P> implements List<E> {
-        @Override
-        public int size() {
-            return 0;
-        }
+    static <T> void method(T t,  List<T> list) {
 
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @Override
-        public Iterator<E> iterator() {
-            return null;
-        }
-
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @Override
-        public <T> T[] toArray(T[] a) {
-            return null;
-        }
-
-        @Override
-        public boolean add(E e) {
-            return false;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(Collection<? extends E> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(int index, Collection<? extends E> c) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-
-        @Override
-        public E get(int index) {
-            return null;
-        }
-
-        @Override
-        public E set(int index, E element) {
-            return null;
-        }
-
-        @Override
-        public void add(int index, E element) {
-
-        }
-
-        @Override
-        public E remove(int index) {
-            return null;
-        }
-
-        @Override
-        public int indexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public int lastIndexOf(Object o) {
-            return 0;
-        }
-
-        @Override
-        public ListIterator<E> listIterator() {
-            return null;
-        }
-
-        @Override
-        public ListIterator<E> listIterator(int index) {
-            return null;
-        }
-
-        @Override
-        public List<E> subList(int fromIndex, int toIndex) {
-            return null;
-        }
     }
 
-    public static void main(String[] args) {
-        List<String> s1 = new MyList<String, Integer>();
-        List<String> s2 = new MyList<String, String>();
 
-        
+    public static void main(String[] args) {
+      Generics.<Integer>method(1, Arrays.asList(1,2,3));
+
+      List<String> str = new ArrayList<>(); //java7
+      List<String> c = Collections.emptyList();//arguments가 없어도 알아서 타입을 추론 해줌.
     }
 
 }
