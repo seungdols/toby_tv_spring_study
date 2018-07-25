b3906eb1bc770ea03a8f40c48cfcb2d99b3a50ea
seungdols <seungdols0822@gmail.com>
2018-07-26 01:17:58 +0900

    IntersectionType, Lambda 활용한 동적 기능 추가

--- toby-generics/src/main/java/com/tistory/seungdols/generics/IntersectionType.java
+++ toby-generics/src/main/java/com/tistory/seungdols/generics/IntersectionType.java
@@ -11,32 +11,104 @@ import java.util.function.Function;
  */
 public class IntersectionType {
 
+    interface Pair<T> {
+        T getFirst();
+
+        T getSecond();
+
+        void setFirst(T first);
+
+        void setSecond(T second);
+    }
+
+    static class Name implements Pair<String> {
+
+        String firstName;
+        String lastName;
+
+        public Name(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        @Override
+        public String getFirst() {
+            return this.firstName;
+        }
+
+        @Override
+        public String getSecond() {
+            return this.lastName;
+        }
+
+        @Override
+        public void setFirst(String first) {
+            this.firstName = first;
+        }
+
+        @Override
+        public void setSecond(String second) {
+            this.lastName = second;
+        }
+    }
+
+    interface ForwardingPair<T> extends DelegateTo<Pair<T>>, Pair<T> {
+        default T getFirst() {
+            return delegate().getFirst();
+        }
+
+        default T getSecond() {
+            return delegate().getSecond();
+        }
+
+        default void setFirst(T first) {
+            delegate().setFirst(first);
+        }
+
+        default void setSecond(T second) {
+            delegate().setSecond(second);
+        }
+    }
+
+
     interface DelegateTo<T> {
         T delegate();
-    }
-
-    interface Hello extends DelegateTo<String> {
-        default void hello() {
-            System.out.println("Hello " + delegate());
-        }
-    }
-
-    interface Uppercase extends DelegateTo<String> {
-        default void uppercase() {
-            System.out.println(delegate().toUpperCase());
-        }
-    }
-
-    public static void main(String[] args) {
-        run((DelegateTo<String> & Hello & Uppercase) () -> "Seungdols Company", o -> {
-            o.hello();
-            o.uppercase();
-        });
     }
 
     //Callback 방식
     private static <T extends DelegateTo<S>, S> void run(T t, Consumer<T> consumer) {
         consumer.accept(t);
+    }
+
+    interface Convertable<T> extends DelegateTo<Pair<T>> {//동적기능 추가용 interface
+        default void convert(Function<T,T> mapper) {
+            Pair<T> pair = delegate();
+            pair.setFirst(mapper.apply(pair.getFirst()));
+            pair.setSecond(mapper.apply(pair.getSecond()));
+        }
+    }
+
+    interface Printable<T> extends DelegateTo<Pair<T>> {//동적기능 추가용 interface
+        default void print() {
+            System.out.println(delegate().getFirst() + " " + delegate().getSecond());
+        }
+    }
+
+    static <T> void print(Pair<T> pair) {
+        System.out.println(pair.getFirst() + " " + pair.getSecond());
+    }
+
+    public static void main(String[] args) {
+        Pair<String> name = new Name("Seungdols", "Company");
+        run((ForwardingPair<String> & Convertable<String> & Printable<String>)() -> name, o -> {
+//            System.out.println(o.getFirst());
+//            System.out.println(o.getSecond());
+            o.print();
+            o.convert(s->s.toUpperCase());
+            o.print();
+            o.convert(s->s.substring(0, 3));
+            o.print();
+        });
     }
 
 }
