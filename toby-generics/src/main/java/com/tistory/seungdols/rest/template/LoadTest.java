c43ff47dd7508ae7e7183958b5bb908acaff830d
seungdols <seungdols0822@gmail.com>
2018-08-22 00:50:50 +0900

    Toby TV: Reactive Programming 5  - 비동기 RestTemplate과 비동기 MVC/Servlet
    * 기본 구조 예제

---
+++ toby-generics/src/main/java/com/tistory/seungdols/rest/template/LoadTest.java
@@ -0,0 +1,51 @@
+package com.tistory.seungdols.rest.template;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @PACKAGE com.tistory.seungdols.rest.template
+ * @AUTHOR seungdols
+ * @DATE 2018. 8. 22.
+ */
+@Slf4j
+public class LoadTest {
+
+    static AtomicInteger counter = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(100);
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8080/rest";
+
+        StopWatch main = new StopWatch();
+        main.start();
+
+        for (int i = 0; i < 100; i++) {
+            es.execute(() -> {
+                int idx = counter.addAndGet(1);
+                log.debug("Thread {}", idx);
+
+                StopWatch stopWatch = new StopWatch();
+                stopWatch.start();
+
+                restTemplate.getForObject(url, String.class);
+                stopWatch.stop();
+                log.info("Elapsed: {} {}", idx, stopWatch.getTotalTimeSeconds());
+            });
+        }
+
+        es.shutdown();
+        es.awaitTermination(100, TimeUnit.SECONDS);
+
+        main.stop();
+        log.info("Total: {}", main.getTotalTimeSeconds());
+    }
+
+}
---
+++ toby-generics/src/main/java/com/tistory/seungdols/rest/template/RestTemplateApplication.java
@@ -0,0 +1,29 @@
+package com.tistory.seungdols.rest.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @PACKAGE com.tistory.seungdols.rest.template
+ * @AUTHOR seungdols
+ * @DATE 2018. 8. 21.
+ */
+@SpringBootApplication
+public class RestTemplateApplication {
+
+    @RestController
+    public static class MyController {
+
+        @RequestMapping("/rest")
+        public String rest() {
+            return "hello"; //http body 내에 포함 되게 된다.
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateApplication.class, args);
+    }
+}
