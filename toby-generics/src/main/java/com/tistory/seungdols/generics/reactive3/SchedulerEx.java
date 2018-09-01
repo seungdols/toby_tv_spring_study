872ffe6ea2dda4f8ac7e40ad313ebf1cedea6cd2
seungdols <seungdols0822@gmail.com>
2018-09-02 00:49:30 +0900

    Toby TV: Reactive Streams (3) - Schedulers 초기 예제 구성

---
+++ toby-generics/src/main/java/com/tistory/seungdols/generics/reactive3/SchedulerEx.java
@@ -0,0 +1,61 @@
+package com.tistory.seungdols.generics.reactive3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+/**
+ * @PACKAGE com.tistory.seungdols.generics.reactive3
+ * @AUTHOR seungdols
+ * @DATE 2018. 9. 2.
+ */
+@Slf4j
+public class SchedulerEx {
+
+    //Reactive Streams
+    public static void main(String[] args) {
+        Publisher<Integer> pub = sub -> {
+            sub.onSubscribe(new Subscription() {
+                @Override
+                public void request(long n) {
+                    sub.onNext(1);
+                    sub.onNext(2);
+                    sub.onNext(3);
+                    sub.onNext(4);
+                    sub.onNext(5);
+                    sub.onComplete();
+                }
+
+                @Override
+                public void cancel() {
+
+                }
+            });
+        };
+
+        pub.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscribe()");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                log.debug("onNext: {}", integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.debug("onError: {}", t.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete()");
+            }
+        });
+
+    }
+}
