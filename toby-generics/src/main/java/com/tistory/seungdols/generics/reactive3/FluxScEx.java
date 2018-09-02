ceb785f51821c01de21f02c0f25245bd368f925c
seungdols <seungdols0822@gmail.com>
2018-09-02 19:14:03 +0900

    Toby TV: Reactive Streams (3) - schedulers: Flux.interval and take 예제

--- toby-generics/src/main/java/com/tistory/seungdols/generics/reactive3/FluxScEx.java
+++ toby-generics/src/main/java/com/tistory/seungdols/generics/reactive3/FluxScEx.java
@@ -1,7 +1,8 @@
 package com.tistory.seungdols.generics.reactive3;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
 
 /**
  * @PACKAGE com.tistory.seungdols.generics.reactive3
@@ -10,13 +11,23 @@ import reactor.core.scheduler.Schedulers;
  */
 public class FluxScEx {
 
-    public static void main(String[] args) {
-        Flux.range(1, 10)
+    public static void main(String[] args) throws InterruptedException {
+/*        Flux.range(1, 10)
             .publishOn(Schedulers.newSingle("pub"))
             .log()
             .subscribeOn(Schedulers.newSingle("sub"))
             .subscribe(System.out::println);
 
+        System.out.println("EXIT");
+*/
+        // Deamon Thread들은 남아 있더라도, 죽인다.
+        // User Thread는 하나라도 있으면, 죽이지 않는다.
+        Flux.interval(Duration.ofMillis(500))
+            .take(10)
+            .log()
+            .subscribe(System.out::println);
+
+        TimeUnit.SECONDS.sleep(10);
         System.out.println("EXIT");
     }
 }
