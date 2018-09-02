f8350581dfae92bf7cf994fdc858878af4432603
seungdols <seungdols0822@gmail.com>
2018-09-02 19:14:22 +0900

    Toby TV: Reactive Streams (3) - schedulers: take 예제에 대한 구현

---
+++ toby-generics/src/main/java/com/tistory/seungdols/generics/reactive3/IntervalEx.java
@@ -0,0 +1,100 @@
+package com.tistory.seungdols.generics.reactive3;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+/**
+ * @PACKAGE com.tistory.seungdols.generics.reactive3
+ * @AUTHOR seungdols
+ * @DATE 2018. 9. 2.
+ */
+@Slf4j
+public class IntervalEx {
+
+    public static void main(String[] args) {
+        Publisher<Integer> pub = sub -> {
+            sub.onSubscribe(new Subscription() {
+                int no = 0;
+                boolean cancelled = false;
+
+                @Override
+                public void request(long n) {
+                    ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+                    exec.scheduleAtFixedRate(() -> {
+                        if (cancelled) {
+                            exec.shutdown();
+                            return;
+                        }
+                        sub.onNext(no++);
+                    }, 0, 500, TimeUnit.MILLISECONDS);
+                }
+
+                @Override
+                public void cancel() {
+                    cancelled = true;
+                    log.debug("cancel()");
+                }
+            });
+        };
+
+        Publisher<Integer> takePub = sub -> {
+            pub.subscribe(new Subscriber<Integer>() {
+                int count = 0;
+                Subscription subsc;
+
+                @Override
+                public void onSubscribe(Subscription s) {
+                    subsc = s;
+                    sub.onSubscribe(s);
+                }
+
+                @Override
+                public void onNext(Integer integer) {
+                    sub.onNext(integer);
+                    if (++count > 5) {
+                        subsc.cancel();
+                    }
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    sub.onError(t);
+                }
+
+                @Override
+                public void onComplete() {
+                    sub.onComplete();
+                }
+            });
+        };
+
+        takePub.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscribe()");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                log.debug("onNext: {}", integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.debug("onError: {}", t.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete()");
+            }
+        });
+    }
+
+}
