ba9ba53bbd5d8cc6fa5bbedd82d90172a48b8acd
seungdols <seungdols0822@gmail.com>
2019-01-05 11:13:05 +0900

    Add subOnPublisher Example

---
+++ toby-schedulers/src/main/java/spring/SpringApplication.java
@@ -0,0 +1,9 @@
+package spring;
+
+/**
+ * @PACKAGE spring
+ * @AUTHOR seungdols
+ * @DATE 05/01/2019
+ */
+public class SpringApplication {
+}
---
+++ toby-schedulers/src/main/java/spring/seungdols/schedulers/Schedulers.java
@@ -0,0 +1,68 @@
+package spring.seungdols.schedulers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @PACKAGE spring.seungdols.schedulers
+ * @AUTHOR seungdols
+ * @DATE 05/01/2019
+ */
+@Slf4j
+public class Schedulers {
+    public static void main(String[] args) {
+        Publisher<Integer> pub = sub -> {
+            sub.onSubscribe(new Subscription() {
+                @Override
+                public void request(long n) {
+                    log.debug("reqeust()");
+                    sub.onNext(1);
+                    sub.onNext(2);
+                    sub.onNext(3);
+                    sub.onNext(4);
+                    sub.onNext(5);
+                    sub.onComplete();
+                }
+
+                @Override
+                public void cancel() {
+
+                }
+            });
+        };
+
+        Publisher<Integer> subOnPub = sub -> {
+            ExecutorService es = Executors.newSingleThreadExecutor();
+            es.execute(() -> pub.subscribe(sub));
+        };
+
+        subOnPub.subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                log.debug("onNext: {}", integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.debug("onError: {}", t);
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        });
+        System.out.println("Exit");
+    }
+}
